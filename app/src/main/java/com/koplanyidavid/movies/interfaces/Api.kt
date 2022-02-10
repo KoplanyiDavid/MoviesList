49bb9affc96d8ae671f44887ad2f79c9281bced4
Dávid Koplányi <koplanyidavid@gmail.com>
2022-02-10 18:05:12 +0100

    Hello

---
+++ app/src/main/java/com/koplanyidavid/movies/interfaces/Api.kt
@@ -0,0 +1,28 @@
+package com.koplanyidavid.movies.interfaces
+
+import com.koplanyidavid.movies.data.GetMoviesResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface Api {
+
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key") apiKey: String = "8d61230b01928fe55a53a48a41dc839b",
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+
+    @GET("movie/top_rated")
+    fun getTopRatedMovies(
+        @Query("api_key") apiKey: String = "8d61230b01928fe55a53a48a41dc839b",
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+
+    @GET("movie/upcoming")
+    fun getUpcomingMovies(
+        @Query("api_key") apiKey: String = "8d61230b01928fe55a53a48a41dc839b",
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+}
